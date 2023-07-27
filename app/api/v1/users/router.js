14fab92062408dce0862c101d352e1f2ccd68d59
gbrn7 <mraygbrn@gmail.com>
2023-07-27 13:35:33 +0700

    feat(auth) created api middleware authentication and authorization

--- app/api/v1/users/router.js
+++ app/api/v1/users/router.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const { activeUser, signUp, signIn, createCmsAdmin, signInCmsAdmin, updateUser, getUsers, getAdmins, countUsers } = require('./controller');
+const { authenticateUser, authorizeRoles } = require('../../../middlewares/auth');
 const router = express();
 
 //user Authorization
-router.get('/users', getUsers);
-
 router.post('/users/auth/signup', signUp);
 
 router.post('/users/auth/signin', signIn);
 
-router.put('/users/:id', updateUser);
-
-//Admin Authorization
-router.get('/admins', getAdmins);
-
-router.get('/admins/Count', countUsers);
-
-router.post('/users/admin/create', createCmsAdmin);
-
 router.put('/active', activeUser);
 
+router.put('/users/:id', authenticateUser, authorizeRoles('user'), updateUser);
+
+//Admin Authorization
 router.post('/admin/auth/signin', signInCmsAdmin);
+
+router.get('/users', authenticateUser, authorizeRoles('superAdmin', 'admin'), getUsers);
+
+router.get('/admins', authenticateUser, authorizeRoles('superAdmin'), getAdmins);
+
+router.get('/admins/Count', authenticateUser, authorizeRoles('superAdmin', 'admin'), countUsers);
+
+router.post('/users/admin/create', authenticateUser, authorizeRoles('superAdmin'), createCmsAdmin);
 
 module.exports = router;
---
+++ app/middlewares/auth.js
@@ -0,0 +1,49 @@
+const { UnauthenticatedError, UnauthorizedError } = require("../errors");
+const { isTokenValid } = require("../utils");
+
+const authenticateUser = async (req, res, next) => {
+  try {
+    let token;
+
+    //check header
+    const authHeader = req.headers.authorization;
+
+    if (authHeader && authHeader.startsWith('Bearer')) {
+      token = authHeader.split(' ')[1];
+    }
+
+    if (!token) throw new UnauthenticatedError('Authentication invalid');
+
+    const payload = isTokenValid({ token });
+
+    //Attach user data to req object
+    req.user = {
+      name: payload.name,
+      role: payload.role,
+      email: payload.email,
+      address: payload.address,
+      status: payload.status,
+      phone_num: payload.phone_num,
+    }
+
+    //head to next function with new req
+    next();
+  } catch (error) {
+    //head to router that receive err argument
+    next(error);
+  }
+}
+
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) throw new UnauthorizedError('Unauthorized to access this route');
+
+    next();
+  }
+}
+
+module.exports = {
+  authenticateUser,
+  authorizeRoles,
+}
