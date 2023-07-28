cfc1d7e3911a1988996a1cbd73d47e947f3e6a42
gbrn7 <mraygbrn@gmail.com>
2023-07-28 10:54:21 +0700

    feat(auth) create authorizeAccessData

--- app/api/v1/users/router.js
+++ app/api/v1/users/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { activeUser, signUp, signIn, createCmsAdmin, signInCmsAdmin, updateUser, getUsers, getAdmins, countUsers, getDataUser, getDataAdmin, updateAdmin } = require('./controller');
-const { authenticateUser, authorizeRoles } = require('../../../middlewares/auth');
+const { authenticateUser, authorizeRoles, authorizeAccessData } = require('../../../middlewares/auth');
 const router = express();
 
 //user Authorization
@@ -10,18 +10,18 @@ router.post('/users/auth/signin', signIn);
 
 router.put('/active', activeUser);
 
-router.get('/users/:id', authenticateUser, authorizeRoles('user'), getDataUser);
+router.get('/users/:id', authenticateUser, authorizeRoles('user'), authorizeAccessData, getDataUser);
 
-router.put('/users/:id', authenticateUser, authorizeRoles('user'), updateUser);
+router.put('/users/:id', authenticateUser, authorizeRoles('user'), authorizeAccessData, authorizeAccessData, updateUser);
 
 //Admin Authorization
 router.post('/admins/auth/signin', signInCmsAdmin);
 
 router.get('/users', authenticateUser, authorizeRoles('superAdmin', 'admin'), getUsers);
 
-router.get('/admins/:id', authenticateUser, authorizeRoles('superAdmin', 'admin'), getDataAdmin);
+router.get('/admins/:id', authenticateUser, authorizeRoles('superAdmin', 'admin'), authorizeAccessData, getDataAdmin);
 
-router.put('/admins/:id', authenticateUser, authorizeRoles('admin', 'superAdmin'), updateAdmin);
+router.put('/admins/:id', authenticateUser, authorizeRoles('superAdmin', 'admin'), authorizeAccessData, updateAdmin);
 
 router.get('/admins', authenticateUser, authorizeRoles('superAdmin'), getAdmins);
 
--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -16,25 +16,14 @@ const authenticateUser = async (req, res, next) => {
     const payload = isTokenValid({ token });
 
     //Attach user data to req object
-    if (payload.role === 'user') {
-      req.user = {
-        name: payload.name,
-        role: payload.role,
-        email: payload.email,
-        address: payload.address,
-        status: payload.status,
-        phone_num: payload.phone_num,
-      }
-    } else {
-      req.user = {
-        userId: payload.userId,
-        name: payload.name,
-        role: payload.role,
-        email: payload.email,
-        address: payload.address,
-        status: payload.status,
-        phone_num: payload.phone_num,
-      }
+    req.user = {
+      userId: payload.userId,
+      name: payload.name,
+      role: payload.role,
+      email: payload.email,
+      address: payload.address,
+      status: payload.status,
+      phone_num: payload.phone_num,
     }
 
     //head to next function with new req
@@ -48,13 +37,19 @@ const authenticateUser = async (req, res, next) => {
 
 const authorizeRoles = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) throw new UnauthorizedError('Unauthorized to access this route');
+    if (!roles.includes(req.user.role) || req.user.status === 'not active') throw new UnauthorizedError('Unauthorized to access this route');
 
     next();
   }
 }
 
+const authorizeAccessData = (req, res, next) => {
+  if (req.user.role !== 'superAdmin' && req.user.userId !== req.params.id) throw new UnauthorizedError('Unauthorized to access data');
+  next();
+}
+
 module.exports = {
   authenticateUser,
   authorizeRoles,
+  authorizeAccessData,
 }
