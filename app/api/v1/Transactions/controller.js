47705ecdd2ba0e295acc358b52423c8881ba7b89
gbrn7 <mraygbrn@gmail.com>
2023-07-31 13:54:27 +0700

    feat (api) transactions api initializations

---
+++ app/api/v1/Transactions/controller.js
@@ -0,0 +1,200 @@
+const { StatusCodes } = require('http-status-codes');
+
+const index = async (req, res, next) => {
+  try {
+    const result = await getAllTransaction(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (error) {
+    console.log('first')
+    next(error);
+  }
+}
+
+const create = async (req, res, next) => {
+  try {
+    const result = await createTransaction(req);
+
+    res.status(StatusCodes.CREATED).json({
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+const find = async (req, res, next) => {
+  try {
+    const result = await findTransaction(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const update = async (req, res, next) => {
+  try {
+    const result = await updateTransaction(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const destroy = async (req, res, next) => {
+  try {
+    const result = await deleteTransaction(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const editStatus = async (req, res, next) => {
+  try {
+    const result = await editStatusTransaction(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const readRevenue = async (req, res, next) => {
+  try {
+    const result = await getRevenueTrans(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const readPendingTrans = async (req, res, next) => {
+  try {
+    const result = await getRevenueTrans(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const readHighestSalesProduct = async (req, res, next) => {
+  try {
+    const result = await getHighestSalesProduct(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const readLowestSalesProduct = async (req, res, next) => {
+  try {
+    const result = await getLowestSalesProduct(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const readNewTransactions = async (req, res, next) => {
+  try {
+    const result = await getNewTransactions(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const readNewSchedules = async (req, res, next) => {
+  try {
+    const result = await getNewSchedules(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const readSchedules = async (req, res, next) => {
+  try {
+    const result = await getSchedules(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const updateShipment = async (req, res, next) => {
+  try {
+    const result = await updateShipmentStatus(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+const updatedeliveryReceipt = async (req, res, next) => {
+  try {
+    const result = await updatedShipmentReceipt(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  index,
+  create,
+  find,
+  update,
+  destroy,
+  editStatus,
+  readRevenue,
+  readPendingTrans,
+  readHighestSalesProduct,
+  readLowestSalesProduct,
+  readNewTransactions,
+  readNewSchedules,
+  readSchedules,
+  updateShipment,
+  updatedeliveryReceipt,
+}
---
+++ app/api/v1/Transactions/model.js
@@ -0,0 +1,136 @@
+const mongoose = require('mongoose');
+const { model, Schema, Types } = mongoose;
+
+const orderDetailSchema = new Schema({
+  product: {
+    type: Types.ObjectId,
+    required: true,
+  },
+  qty: {
+    type: Number,
+    required: true,
+  }
+});
+
+const domainSchema = new Schema({
+  city_id: {
+    type: String,
+    required: true,
+  },
+  province_id: {
+    type: String,
+    required: true,
+  },
+  province: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city_name: {
+    type: String,
+    required: true,
+  },
+  postal_code: {
+    type: String,
+    required: true,
+  },
+});
+
+const expeditionSchema = new Schema({
+  origin_details: {
+    type: domainSchema,
+    required: true,
+  },
+  destination_details: {
+    type: domainSchema,
+    required: true,
+  },
+  courierDetail: {
+    code: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    costs:
+    {
+      service: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      cost:
+      {
+        value: {
+          type: number,
+          required: true,
+        },
+        etd: {
+          type: String,
+          required: true,
+        },
+        note: {
+          type: String,
+        }
+      }
+    },
+  },
+  shipment_status: {
+    type: String,
+    required: [true, 'Payment status required'],
+  },
+  Resi: {
+    type: String,
+  },
+})
+
+const transactionModel = new Schema({
+  userId: {
+    type: Types.ObjectId,
+    ref: 'user',
+    required: [true, 'The user id is required'],
+  },
+  address: {
+    type: String,
+    required: [true, 'the address for transaction is required'],
+  },
+  total: {
+    type: Number,
+    required: [true, 'total transactions is required'],
+  },
+  total: {
+    type: Number,
+    required: [true, 'total transactions is required'],
+  },
+  expedition: {
+    type: expeditionSchema,
+    required: [true, 'expedition data is required'],
+  },
+  transactions_code: {
+    type: String,
+    required: [true, 'transaction code is required'],
+  },
+  transaction_status: {
+    type: String,
+    required: [true, 'Payment status required'],
+  },
+  orderDetails: {
+    type: [orderDetailSchema],
+    required: [true, 'the order details id required '],
+  },
+},
+  {
+    timestamps: true
+  }
+);
+
+
+
+module.exports = model('transaction', transactionModel);
